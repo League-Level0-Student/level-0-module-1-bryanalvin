e832e2bb200dd164e730d9eabf315fedabfbafee
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-09 17:46:21 -0700

    the riddler

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -1,25 +1,30 @@
 package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
+		int crash = 0;
+		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddleshat
+		String answer = JOptionPane.showInputDialog("You are my brother but i am not your brother what am i");
+		if (answer.equals("sis")) {
+			JOptionPane.showMessageDialog(null, "OBJECTION!");
+			// 5. Otherwise, say "wrong" and tell them the answer
+			
+			// 6. Add some more riddles
+			String answer = JOptionPane.showInputDialog("You are a boi But i am not a boi what am i");
+			if (answer.equals("girl"));			// 2. Make a pop up to show the score.
 		
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You Suck");
+		}
+		JOptionPane.showMessageDialog(null, crash);
 	}
 }
-
