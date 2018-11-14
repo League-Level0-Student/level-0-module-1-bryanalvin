255a8d32e5f1414b1aec8cd7a23bb7e09074e768
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-13 17:21:24 -0800

    Badgers

---
+++ src/G.java
@@ -0,0 +1,54 @@
+
+public class G {
+	public static void main(String[] args) {
+for (int j = 0; j < 4; j++) {
+	
+
+		for (int i = 0; i < 8; i++) {
+			System.out.println("Badger");
+		}
+		
+		for(int i = 0; i < 2; i++) {
+			System.out.println("Mushroom");
+			
+		for (int k = 0; k < 5; k++) {
+			System.out.println("Snake");
+		
+		}
+		}
+	}
+	
+			}
+}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
